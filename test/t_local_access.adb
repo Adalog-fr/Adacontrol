876ca74b85f0763440107f8db124b631d7f87071
JP Rosen <rosen@adalog.fr>
2011-09-26 12:55:06 +0200

    Converted test for local_access to LF terminators

--- test/t_local_access.adb
+++ test/t_local_access.adb
@@ -1,36 +1,36 @@
 package body T_Local_Access is
    V1 : A_Int;
    V2 : C_Int;
-   V3 : A_Proc;  
+   V3 : A_Proc;
    V4 : A_Func;
    V5 : A_PProc;
    V6 : A_PFunc;
 
-   procedure Proc is 
+   procedure Proc is
    begin
       null;
    end;
-   
-   function Func return Integer is 
-   begin 
+
+   function Func return Integer is
+   begin
       return 1;
    end;
-   
+
    procedure Local is
       V : aliased Integer;
       C : aliased constant Integer := 1;
-      
-      procedure Proc is 
+
+      procedure Proc is
       begin
 	 null;
       end;
-      
-      function Func return Integer is 
-      begin 
+
+      function Func return Integer is
+      begin
 	 return 1;
       end;
       function Other_Func return Integer renames Func;
-      
+
       protected type T_Sema is
 	 entry P;
 	 procedure V;
@@ -50,9 +50,9 @@ package body T_Local_Access is
 	    return 1;
 	 end Length;
       end T_Sema;
-      
+
       Sema : T_Sema;
-    
+
    begin
       V1 := V'Unchecked_Access;              -- Local_Access variable
       V2 := C'Unchecked_Access;              -- Local_Access constant
@@ -61,8 +61,8 @@ package body T_Local_Access is
       V4 := Other_Func'Unrestricted_Access;  -- Local_Access function
       V5 := Sema.V'Unrestricted_Access;      -- Local_Access protected procedure
       V6 := Sema.Length'Unrestricted_Access; -- Local_Access protected function
-   end Local;   
-   
+   end Local;
+
 begin
    V1 := V'Access;    -- OK
    V2 := C'Access;    -- OK
