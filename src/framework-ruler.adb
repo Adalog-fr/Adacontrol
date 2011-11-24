e6df5ac4642239fc21661031236a649d78270e6c
JP Rosen <rosen@adalog.fr>
2011-11-24 12:47:33 +0100

    Null procedures now correctly treated as a scope

--- src/framework-ruler.adb
+++ src/framework-ruler.adb
@@ -357,6 +357,7 @@ package body Framework.Ruler is
             case Declaration_Kind (Element) is
                when A_Function_Declaration
                   | A_Procedure_Declaration
+                  | A_Null_Procedure_Declaration
                   | An_Entry_Declaration
                   | A_Task_Type_Declaration
                   | A_Single_Task_Declaration
@@ -538,6 +539,7 @@ package body Framework.Ruler is
          when A_Declaration =>
             case Declaration_Kind (Element) is
                when A_Procedure_Declaration   -- Any change to this list must be reflected in Framework.Symbol_Table
+                  | A_Null_Procedure_Declaration
                   | A_Function_Declaration
                   | An_Entry_Declaration
                   | A_Package_Declaration
