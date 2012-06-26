d26345d8abe2dbf60f583dfeae66914a48003c17
JP Rosen <rosen@adalog.fr>
2012-06-26 13:56:35 +0100

    Added support for rules variables

---
+++ src/framework-variables.adb
@@ -0,0 +1,118 @@
+----------------------------------------------------------------------
+--  Framework.Variables - Package body                              --
+--                                                                  --
+--  This software  is (c) The European Organisation  for the Safety --
+--  of Air  Navigation (EUROCONTROL) and Adalog  2004-2012. The Ada --
+--  Controller  is  free software;  you can redistribute  it and/or --
+--  modify  it under  terms of  the GNU  General Public  License as --
+--  published by the Free Software Foundation; either version 2, or --
+--  (at your  option) any later version.  This  unit is distributed --
+--  in the hope  that it will be useful,  but WITHOUT ANY WARRANTY; --
+--  without even the implied warranty of MERCHANTABILITY or FITNESS --
+--  FOR A  PARTICULAR PURPOSE.  See the GNU  General Public License --
+--  for more details.   You should have received a  copy of the GNU --
+--  General Public License distributed  with this program; see file --
+--  COPYING.   If not, write  to the  Free Software  Foundation, 59 --
+--  Temple Place - Suite 330, Boston, MA 02111-1307, USA.           --
+--                                                                  --
+--  As  a special  exception, if  other files  instantiate generics --
+--  from the units  of this program, or if you  link this unit with --
+--  other files  to produce  an executable, this  unit does  not by --
+--  itself cause the resulting executable  to be covered by the GNU --
+--  General  Public  License.   This  exception  does  not  however --
+--  invalidate any  other reasons why the executable  file might be --
+--  covered by the GNU Public License.                              --
+--                                                                  --
+--  This  software is  distributed  in  the hope  that  it will  be --
+--  useful,  but WITHOUT  ANY  WARRANTY; without  even the  implied --
+--  warranty  of  MERCHANTABILITY   or  FITNESS  FOR  A  PARTICULAR --
+--  PURPOSE.                                                        --
+----------------------------------------------------------------------
+
+-- Adalog
+with
+   Utilities,
+   Binary_Map;
+
+package body Framework.Variables is
+
+   package Writers_List is new Binary_Map (Unbounded_Wide_String, Writer_Access);
+   Writers_Map : Writers_List.Map;
+
+
+   ------------------
+   -- Variable_Key --
+   ------------------
+
+   function Variable_Key (Rule_Name : Wide_String; Variable_Name : Wide_String) return Unbounded_Wide_String is
+   -- Utility for the Register_XX_Variable packages
+      use Utilities;
+   begin
+      if Rule_Name = "" then
+         return To_Unbounded_Wide_String (To_Upper (Variable_Name));
+      else
+         return To_Unbounded_Wide_String (To_Upper (Rule_Name & '.' & Variable_Name));
+      end if;
+   end Variable_Key;
+
+   ------------------------------
+   -- Register_String_Variable --
+   ------------------------------
+
+   package body Register_String_Variable is
+      procedure Writer (Value : in Wide_String) is
+      begin
+         Variable := To_Unbounded_Wide_String (Value);
+      end Writer;
+      use Utilities, Writers_List;
+   begin
+      Add (Writers_Map, Variable_Key (Rule_Name, Variable_Name), Writer_Ptr);
+   end Register_String_Variable;
+
+   --------------------------------
+   -- Register_Discrete_Variable --
+   --------------------------------
+
+   package body Register_Discrete_Variable is
+      procedure Writer (Value : in Wide_String) is
+      begin
+         if Value = "" then
+            Variable := Variable_Type'Last;
+         else
+            Variable := Decode (Value);
+         end if;
+      end Writer;
+      use Utilities, Writers_List;
+   begin
+      Add (Writers_Map, Variable_Key (Rule_Name, Variable_Name), Writer_Ptr);
+   end Register_Discrete_Variable;
+
+   -------------------------------
+   -- Register_Special_Variable --
+   -------------------------------
+
+   package body Register_Special_Variable is
+      procedure Writer (Value : in Wide_String) is
+      begin
+         Set_Variable (Value);
+      end Writer;
+      use Utilities, Writers_List;
+   begin
+      Add (Writers_Map, Variable_Key (Rule_Name, Variable_Name), Writer_Ptr);
+   end Register_Special_Variable;
+
+   ------------------
+   -- Set_Variable --
+   ------------------
+
+   procedure Set_Variable (Rule_Id : in Wide_String; Variable : in Wide_String; Value : in Wide_String) is
+      use Utilities, Writers_List;
+   begin
+      Fetch (Writers_Map, Variable_Key (Rule_Id, Variable)) (Value);
+   exception
+      when Not_Present =>
+         -- This exception not visible to clients, transform it
+         raise No_Such_Variable;
+   end Set_Variable;
+
+end Framework.Variables;
---
+++ src/framework-variables.ads
@@ -0,0 +1,85 @@
+----------------------------------------------------------------------
+--  Framework.Variables - Package specification                     --
+--                                                                  --
+--  This software  is (c) The European Organisation  for the Safety --
+--  of Air  Navigation (EUROCONTROL) and Adalog  2004-2012. The Ada --
+--  Controller  is  free software;  you can redistribute  it and/or --
+--  modify  it under  terms of  the GNU  General Public  License as --
+--  published by the Free Software Foundation; either version 2, or --
+--  (at your  option) any later version.  This  unit is distributed --
+--  in the hope  that it will be useful,  but WITHOUT ANY WARRANTY; --
+--  without even the implied warranty of MERCHANTABILITY or FITNESS --
+--  FOR A  PARTICULAR PURPOSE.  See the GNU  General Public License --
+--  for more details.   You should have received a  copy of the GNU --
+--  General Public License distributed  with this program; see file --
+--  COPYING.   If not, write  to the  Free Software  Foundation, 59 --
+--  Temple Place - Suite 330, Boston, MA 02111-1307, USA.           --
+--                                                                  --
+--  As  a special  exception, if  other files  instantiate generics --
+--  from the units  of this program, or if you  link this unit with --
+--  other files  to produce  an executable, this  unit does  not by --
+--  itself cause the resulting executable  to be covered by the GNU --
+--  General  Public  License.   This  exception  does  not  however --
+--  invalidate any  other reasons why the executable  file might be --
+--  covered by the GNU Public License.                              --
+--                                                                  --
+--  This  software is  distributed  in  the hope  that  it will  be --
+--  useful,  but WITHOUT  ANY  WARRANTY; without  even the  implied --
+--  warranty  of  MERCHANTABILITY   or  FITNESS  FOR  A  PARTICULAR --
+--  PURPOSE.                                                        --
+----------------------------------------------------------------------
+
+package Framework.Variables is
+   --
+   -- Management of rules variables
+   --
+
+   type Writer_Access is access procedure (Value : Wide_String);
+   -- Used only for private part of Register_XX_Variable, useless (but harmless) for user
+
+   generic
+      Variable      : in out Ada.Strings.Wide_Unbounded.Unbounded_Wide_String;
+      Rule_Name     : in     Wide_String := "";
+      Variable_Name : in     Wide_String;
+   package Register_String_Variable is
+   private
+      procedure Writer (Value : in Wide_String);
+      Writer_Ptr : constant Writer_Access := Writer'Access;
+   end Register_String_Variable;
+
+
+   generic
+      type Variable_Type is (<>);
+      Variable      : in out Variable_Type;
+      Rule_Name     : in     Wide_String := "";
+      Variable_Name : in     Wide_String;
+      with function Decode (Value : in Wide_String) return Variable_Type is Variable_Type'Wide_Value;
+   package Register_Discrete_Variable is
+   private
+      procedure Writer (Value : in Wide_String);
+      Writer_Ptr : constant Writer_Access := Writer'Access;
+   end Register_Discrete_Variable;
+
+
+   -- For cases where simple assignment is not sufficient, provide the necessary behaviour in
+   -- procedure Set_Variable. If Value is incorrect, the procedure shall raise Constraint_Error.
+   generic
+      with procedure Set_Variable (Value : Wide_String);
+      Rule_Name     : in     Wide_String := "";
+      Variable_Name : in     Wide_String;
+   package Register_Special_Variable is
+   private
+      -- Relay procedure needed, can't take 'Access of formal procedure
+      procedure Writer (Value : in Wide_String);
+      Writer_Ptr : constant Writer_Access := Writer'Access;
+   end;
+
+   ---------------------------------------------------------------------
+   --
+   --  Declarations below this line are for the use of the framework
+   --
+
+   procedure Set_Variable (Rule_Id : in Wide_String; Variable : in Wide_String; Value : in Wide_String);
+   No_Such_Variable : exception;
+
+end Framework.Variables;
