914d4bdd9ff7c4ae69df7ffc8cb937f803401ecf
JP Rosen <rosen@adalog.fr>
2012-11-07 14:08:42 +0000

    Fixed missing case of return  object

--- src/rules-simplifiable_expressions.adb
+++ src/rules-simplifiable_expressions.adb
@@ -653,13 +653,14 @@ package body Rules.Simplifiable_expressions is
                      end case;
 
                   when A_Variable_Declaration
-                    | A_Constant_Declaration
-                    | A_Deferred_Constant_Declaration
-                    | A_Formal_Object_Declaration
-                    | A_Parameter_Specification
-                    | A_Component_Declaration
-                    | An_Object_Renaming_Declaration
-                    =>
+                     | A_Constant_Declaration
+                     | A_Deferred_Constant_Declaration
+                     | A_Return_Object_Declaration
+                     | A_Formal_Object_Declaration
+                     | A_Parameter_Specification
+                     | A_Component_Declaration
+                     | An_Object_Renaming_Declaration
+                     =>
                      null;
 
                   when others =>
