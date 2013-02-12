7ed8b78305560d63069c1f17662f49a23adf4361
JP Rosen <rosen@adalog.fr>
2013-02-12 11:04:40 +0000

    Max_Size: new subrules

--- test/t_max_size.adb
+++ test/t_max_size.adb
@@ -1,69 +1,131 @@
 procedure T_Max_Size is
    V : Integer;
 
+   package Pack1 is
+      A : Integer;
+   end Pack1;                        -- OK
+
+   package Pack2 is
+      B : Integer;
+      procedure P;
+      function F return Integer;
+   end Pack2;                        -- Package_Spec
+
+   package body Pack2 is
+      procedure P is
+      begin
+         null;
+      end P;                         -- OK
+      function F return Integer is begin return 1;  end F; -- OK
+   begin
+      null;
+   end Pack2;                        -- Package_Body
+
+   function F return Integer is
+   begin
+      return 1;
+   end F;                            -- Function_Body
+
    task T is
       entry E;
-   end T;
+   end T;                            -- OK
    task body T is
    begin
       accept E;
       accept E do
          null;
-      end E;
-      accept E do  -- accept
+      end E;                         -- OK
+      accept E do
          null;
          null;
          null;
-      end E;
-   end T;
+      end E;                         -- Accept
+   end T;                            -- Task_Body
+
+   task type TT is
+      entry E1;
+      entry E2;
+   end TT;                           -- Task_Spec
+
+   task body TT is
+   begin
+      null;
+   end TT;                           -- OK
+
+   protected type PT is
+      procedure E1;
+      entry E2;
+   end PT;                           -- Protected_Spec
+
+   protected body PT is
+      procedure E1 is
+      begin
+         null;
+      end E1;                        -- OK
+      entry E2 when True is
+      begin
+         null;
+      end E2;                        -- Entry_Body
+   end PT;                           -- Protected_Body
+
+   protected type P is
+      procedure E1;
+   end P;                            -- OK
+
+   protected body P is
+      procedure E1 is
+      begin
+         null;
+      end E1;                        -- OK
+   end P;                            -- OK
 begin
-   if V = 1 then       -- if
-      if V = 2 then    -- if branch
+   if V = 1 then
+      if V = 2 then
          null;
-      end if;
+      end if;                        -- If_Branch
    elsif V = 2 then
-      null;           -- if branch
       null;
       null;
+      null;                          -- If_Branch
    else
       null;
       null;
-   end if;
+   end if;                           -- If
 
-   case V is          -- case
+   case V is
       when 1 =>
          null;
          null;
       when 2 =>
-         null;        -- case_branch
          null;
          null;
+         null;                       -- Case_Branch
       when others =>
-         case V is    -- case branch
+         case V is
             when others =>
                null;
-         end case;
-   end case;
+         end case;                   -- Case_Branch
+   end case;                         -- Case
 
-   while V /= 0 loop  --OK
+   while V /= 0 loop                 --OK
       null;
    end loop;
-   loop               -- Unnamed_Loop
+   loop
       null;
       null;
-   end loop;
+   end loop;                         -- Unnamed_Loop
 L1: for I in 1 .. 10 loop
       null;
       null;
    end loop L1;
-L2: for I in 1 .. 10 loop -- Loop
+L2: for I in 1 .. 10 loop
       null;
       null;
       null;
       null;
-   end loop L2;
+   end loop L2;                      -- Loop
 
-   begin                 -- Block, Unnamed_Block, Simple_Block
+   begin
       declare
          VVV : Integer;
       begin
@@ -72,9 +134,9 @@ L2: for I in 1 .. 10 loop -- Loop
    exception
       when others =>
          null;
-   end;
+   end;                              -- Block, Unnamed_Block, Simple_Block
 
-B1: begin                 -- Block, Simple_Block
+B1: begin
       declare
          VVV : Integer;
       begin
@@ -83,9 +145,9 @@ B1: begin                 -- Block, Simple_Block
    exception
       when others =>
          null;
-   end B1;
+   end B1;                           -- Block, Simple_Block
 
-   declare                -- Block, Unnamed_Block
+   declare
       VV : Integer;
    begin
       declare
@@ -93,9 +155,9 @@ B1: begin                 -- Block, Simple_Block
       begin
          null;
       end;
-   end;
+   end;                              -- Block, Unnamed_Block
 
-   B2: declare            -- Block
+   B2: declare
       VV : Integer;
    begin
       declare
@@ -103,5 +165,5 @@ B1: begin                 -- Block, Simple_Block
       begin
          null;
       end;
-   end B2;
-end T_Max_Size;
+   end B2;                           -- Block
+end T_Max_Size;                      -- Procedure_Body, Unit
