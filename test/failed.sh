6b3a1b603a52057994c129629108953b837f0ad7
unknown <Jean-Pierre Rosen@Voyager.(none)>
2014-10-13 07:07:25 +0200

    Add new option for failed check: save diff as patch

--- test/failed.sh
+++ test/failed.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+
+PATCHFILE=testpatch.diff
+
 if [ $OSTYPE == "linux-gnu" ] ; then
     WINMERGE='/usr/bin/meld'
+    EMACS=emacs
 else
     WINMERGE='/cygdrive/c/Program Files (x86)/KDiff3/kdiff3.exe'
+    EMACS=emacs.bat
 fi
 
 (cd res
@@ -10,15 +15,18 @@ fi
 	diff=`diff ${test_case} ../ref/${test_case}`
 	if [ "$diff" != "" ]; then
 	    while true ; do
-		echo -n "$test_case: [Diff, Edit, Interactive, Validate, Nothing, Quit]? "
+		echo -n "$test_case: [Diff, Edit, Interactive, save Patch, Validate, Nothing, Quit]? "
 		read REP
 		case $REP in
 	            d | D )
 			diff ../ref/${test_case} ${test_case}  2>/dev/null;;
                     e | E )
-			emacs.bat ${test_case};;
+			${EMACS} ${test_case};;
 	            i | I )
 			"$WINMERGE" ../ref/${test_case} ${test_case} 2>/dev/null;;
+		    p | P )
+			diff --unified ../ref/${test_case} ${test_case}  >>../${PATCHFILE} 2>/dev/null;
+			echo "Patch appended to $PATCHFILE";;
 	            v | V )
 			cp  ${test_case} ../ref/${test_case}
 			break;;
