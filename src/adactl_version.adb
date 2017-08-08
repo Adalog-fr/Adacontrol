ee626811cb00af76f0ef430af72c8bbd211d9978
JP Rosen <rosen@adalog.fr>
2017-08-08 06:39:25 +0100

    Set version to 1.19b5

--- src/adactl_version.adb
+++ src/adactl_version.adb
@@ -29,7 +29,7 @@ with
    Framework.Specific_Plugs;
 function Adactl_Version return Wide_String is
 
-   Version : constant Wide_String := "1.19b4";
+   Version : constant Wide_String := "1.19b5";
 
 begin
    -- Gnat warns that the following condition is always false/true, but
