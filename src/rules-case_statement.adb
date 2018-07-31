c2be4ef5908d4ff54fc5f238d2b53d9febed0451
Rosen <rosen@adalog.fr>
2018-07-31 14:51:34 +0100

    Case_Statement: add entity modifier to all ranges
    removed redundant Enumeration_Range_Span

--- src/rules-case_statement.adb
+++ src/rules-case_statement.adb
@@ -47,33 +47,34 @@ pragma Elaborate (Framework.Language);
 
 package body Rules.Case_Statement is
 
-   use Asis, Framework, Thick_Queries;
+   use Asis, Framework, Framework.Language.Shared_Keys, Thick_Queries;
 
-   type Subrules is (Others_Span, Paths, Range_Span, Enumeration_Range_Span, Values, Values_If_Others);
+   type Subrules is (Others_Span, Paths, Range_Span, Values, Values_If_Others);
    package Subrules_Flag_Utilities is new Framework.Language.Flag_Utilities (Subrules);
 
-   type Usage is array (Subrules) of Control_Kinds_Set;
-   Rule_Used : Usage := (others => (others => False));
+   type Usage is array (Subrules, Discrete_Categories) of Control_Kinds_Set;
+   Rule_Used : Usage := (others => (others => (others => False)));
    Save_Used : Usage;
 
-   Labels : array (Subrules, Control_Kinds) of Ada.Strings.Wide_Unbounded.Unbounded_Wide_String;
+   Labels : array (Subrules, Discrete_Categories, Control_Kinds) of Ada.Strings.Wide_Unbounded.Unbounded_Wide_String;
 
-   Bounds : array (Subrules, Control_Kinds) of Framework.Language.Shared_Keys.Bounds_Values
-     := (others => (others => (0, 0)));
+   Bounds : array (Subrules, Discrete_Categories, Control_Kinds) of Framework.Language.Shared_Keys.Bounds_Values
+     := (others => (others => (others => (0, 0))));
 
    ----------
    -- Help --
    ----------
 
    procedure Help is
-      use Utilities, Framework.Language.Shared_Keys;
+      use Utilities;
    begin
       User_Message ("Rule: "& Rule_Id);
       User_Message ("Controls various sizes related to the case statement");
       User_Message;
-      Subrules_Flag_Utilities.Help_On_Flags (Header => "Parameter(1)   :");
+      Subrules_Flag_Utilities.Help_On_Flags (Header => "Parameter(1)   : [<category>] ");
       User_Message ("Parameter(2..3): <bound> <value>");
       User_Message ("                (at least one parameter required)");
+      Help_On_Categories (Expected => Discrete_Set);
       Help_On_Bounds (Header => "<bound>: ");
    end Help;
 
@@ -82,26 +83,43 @@ package body Rules.Case_Statement is
    -----------------
 
    procedure Add_Control (Ctl_Label : in Wide_String; Ctl_Kind : in Control_Kinds) is
-      use Framework.Language, Framework.Language.Shared_Keys, Subrules_Flag_Utilities;
-      use Ada.Strings.Wide_Unbounded;
+      use Framework.Language, Categories_Utilities, Subrules_Flag_Utilities;
 
-      Subrule_Name : Subrules;
-   begin
+      Subrule_Name   : Subrules;
+      Subrule_Cat    : Categories;
+      Subrule_Bounds : Bounds_Values;
+
+      procedure Add_Cat  (Cat : Categories) is
+         use Ada.Strings.Wide_Unbounded;
+      begin
+         if Rule_Used (Subrule_Name, Cat) (Ctl_Kind) then
+            Parameter_Error (Rule_Id, "rule already specified for " & Control_Kinds'Wide_Image (Ctl_Kind));
+         end if;
+
+         Bounds    (Subrule_Name, Cat, Ctl_Kind)  := Subrule_Bounds;
+         Labels    (Subrule_Name, Cat, Ctl_Kind)  := To_Unbounded_Wide_String (Ctl_Label);
+         Rule_Used (Subrule_Name, Cat) (Ctl_Kind) := True;
+      end Add_Cat;
+
+   begin  -- Add_Control
       if not Parameter_Exists then
          Parameter_Error (Rule_Id, "parameters required");
       end if;
 
+      Subrule_Cat  := Get_Modifier (Required => False, Expected => Discrete_Set, Default => Cat_Any);
       Subrule_Name := Get_Flag_Parameter (Allow_Any => False);
-      if Rule_Used (Subrule_Name) (Ctl_Kind) then
-         Parameter_Error (Rule_Id, "rule already specified for " & Control_Kinds'Wide_Image (Ctl_Kind));
-      end if;
-
       if not Parameter_Exists then
          Parameter_Error (Rule_Id, "two or three parameters required");
       end if;
-      Bounds    (Subrule_Name, Ctl_Kind) := Get_Bounds_Parameters (Rule_Id);
-      Labels    (Subrule_Name, Ctl_Kind) := To_Unbounded_Wide_String (Ctl_Label);
-      Rule_Used (Subrule_Name)(Ctl_Kind) := True;
+
+      Subrule_Bounds := Get_Bounds_Parameters (Rule_Id);
+      if Subrule_Cat = Cat_Any then
+         for C in Discrete_Categories loop
+            Add_Cat (C);
+         end loop;
+      else
+          Add_Cat (Subrule_Cat);
+      end if;
     end Add_Control;
 
    -------------
@@ -113,10 +131,10 @@ package body Rules.Case_Statement is
    begin
       case Action is
          when Clear =>
-            Rule_Used := (others => (others => False));
+            Rule_Used := (others => (others => (others => False)));
          when Suspend =>
             Save_Used := Rule_Used;
-            Rule_Used := (others => (others => False));
+            Rule_Used := (others => (others => (others => False)));
          when Resume =>
             Rule_Used := Save_Used;
       end case;
@@ -129,54 +147,67 @@ package body Rules.Case_Statement is
    procedure Check_Report (Subrule_Name : Subrules;
                            Value        : Biggest_Natural;
                            Message      : Wide_String;
-                           Elem         : Asis.Element)
+                           Elem         : Asis.Element;
+                           Elem_Cat     : Categories)
    is
       use Ada.Strings.Wide_Unbounded;
       use Framework.Reports;
    begin
-      if Rule_Used (Subrule_Name) (Check) and then Value < Bounds (Subrule_Name, Check).Min then
+      if Rule_Used (Subrule_Name, Elem_Cat) (Check)
+        and then Value < Bounds (Subrule_Name, Elem_Cat, Check).Min
+      then
          Report (Rule_Id,
-                 To_Wide_String (Labels (Subrule_Name, Check)),
+                 To_Wide_String (Labels (Subrule_Name, Elem_Cat, Check)),
                  Check,
                  Get_Location (Elem),
                  "too few " & Message
                  & " (" & Biggest_Int_Img (Value) & ')');
-      elsif Rule_Used (Subrule_Name) (Search) and then Value < Bounds (Subrule_Name, Search).Min then
+      elsif Rule_Used (Subrule_Name, Elem_Cat) (Search)
+        and then Value < Bounds (Subrule_Name, Elem_Cat, Search).Min
+      then
          Report (Rule_Id,
-                 To_Wide_String (Labels (Subrule_Name, Search)),
+                 To_Wide_String (Labels (Subrule_Name, Elem_Cat, Search)),
                  Search,
                  Get_Location (Elem),
                  "too few " & Message
                  & " (" & Biggest_Int_Img (Value) & ')');
       end if;
 
-      if Rule_Used (Subrule_Name) (Count) and then Value < Bounds (Subrule_Name, Count).Min then
+      if Rule_Used (Subrule_Name, Elem_Cat) (Count)
+        and then Value < Bounds (Subrule_Name, Elem_Cat, Count).Min
+      then
          Report (Rule_Id,
-                 To_Wide_String (Labels (Subrule_Name, Count)),
+                 To_Wide_String (Labels (Subrule_Name, Elem_Cat, Count)),
                  Count,
                  Get_Location (Elem),
                  "");
       end if;
 
-      if Rule_Used (Subrule_Name) (Check) and then Value > Bounds (Subrule_Name, Check).Max then
+      if Rule_Used (Subrule_Name, Elem_Cat) (Check)
+        and then Value > Bounds (Subrule_Name, Elem_Cat, Check).Max
+      then
          Report (Rule_Id,
-                 To_Wide_String (Labels (Subrule_Name, Check)),
+                 To_Wide_String (Labels (Subrule_Name, Elem_Cat, Check)),
                  Check,
                  Get_Location (Elem),
                  "too many " & Message
                  & " (" & Biggest_Int_Img (Value) & ')');
-      elsif Rule_Used (Subrule_Name) (Search) and then Value > Bounds (Subrule_Name, Search).Max then
+      elsif Rule_Used (Subrule_Name, Elem_Cat) (Search)
+        and then Value > Bounds (Subrule_Name, Elem_Cat, Search).Max
+      then
          Report (Rule_Id,
-                 To_Wide_String (Labels (Subrule_Name, Search)),
+                 To_Wide_String (Labels (Subrule_Name, Elem_Cat, Search)),
                  Search,
                  Get_Location (Elem),
                  "too many " & Message
                  & " (" & Biggest_Int_Img (Value) & ')');
       end if;
 
-      if Rule_Used (Subrule_Name) (Count) and then Value > Bounds (Subrule_Name, Count).Max then
+      if Rule_Used (Subrule_Name, Elem_Cat) (Count)
+        and then Value > Bounds (Subrule_Name, Elem_Cat, Count).Max
+      then
          Report (Rule_Id,
-                 To_Wide_String (Labels (Subrule_Name, Count)),
+                 To_Wide_String (Labels (Subrule_Name, Elem_Cat, Count)),
                  Count,
                  Get_Location (Elem),
                  "");
@@ -192,6 +223,7 @@ package body Rules.Case_Statement is
       use Framework.Reports;
 
       Non_Evaluable : exception;
+      Case_Cat      : Categories;
 
       -- Compute the number of cases covered by all case alternatives
       -- (including discrete ranges) excluding the "when others" alternative
@@ -281,9 +313,10 @@ package body Rules.Case_Statement is
          end if;
 
          Check_Report (Others_Span,
-                       Value   => Subtype_Span - Count_Non_Others_Choices (Case_Paths),
-                       Message => "values covered by ""others"" in case statement",
-                       Elem    => Case_Paths (Case_Paths'Last));
+                       Value    => Subtype_Span - Count_Non_Others_Choices (Case_Paths),
+                       Message  => "values covered by ""others"" in case statement",
+                       Elem     => Case_Paths (Case_Paths'Last),
+                       Elem_Cat => Case_Cat);
 
       exception
          when Non_Evaluable =>
@@ -315,15 +348,17 @@ package body Rules.Case_Statement is
          end if;
 
          Check_Report (Values,
-                       Value   => Subtype_Span,
-                       Message => "values for subtype of selector in case statement",
-                       Elem    => Statement);
+                       Value    => Subtype_Span,
+                       Message  => "values for subtype of selector in case statement",
+                       Elem     => Statement,
+                       Elem_Cat => Case_Cat);
 
          if Has_Others then
             Check_Report (Values_If_Others,
-                          Value   => Subtype_Span,
-                          Message => "values for subtype of selector in case statement with ""others""",
-                          Elem    => Statement);
+                          Value    => Subtype_Span,
+                          Message  => "values for subtype of selector in case statement with ""others""",
+                          Elem     => Statement,
+                          Elem_Cat => Case_Cat);
          end if;
 
       exception
@@ -334,28 +369,35 @@ package body Rules.Case_Statement is
       procedure Process_Min_Paths is
       begin
          Check_Report (Paths,
-                       Value   => Statement_Paths (Statement)'Length,
-                       Message => "paths in case statement",
-                       Elem    => Statement);
+                       Value    => Statement_Paths (Statement)'Length,
+                       Message  => "paths in case statement",
+                       Elem     => Statement,
+                       Elem_Cat => Case_Cat);
       end Process_Min_Paths;
 
    begin  -- Process_Case_Statement
-      if Rule_Used = (Subrules => (Control_Kinds => False)) then
+      if Rule_Used = (Subrules => (Discrete_Categories => (Control_Kinds => False))) then
          return;
       end if;
       Rules_Manager.Enter (Rule_Id);
 
-      if   Rule_Used (Values)           /= (Control_Kinds => False)
-        or Rule_Used (Values_If_Others) /= (Control_Kinds => False)
+      Case_Cat := Matching_Category (Case_Expression (Statement),
+                                     From_Cats          => Discrete_Set,
+                                     Follow_Derived     => True,
+                                     Privacy            => Follow_User_Private,
+                                     Separate_Extension => False);
+
+      if   Rule_Used (Values, Case_Cat)           /= (Control_Kinds => False)
+        or Rule_Used (Values_If_Others, Case_Cat) /= (Control_Kinds => False)
       then
          Process_Max_Values;
       end if;
 
-      if Rule_Used (Paths) /= (Control_Kinds => False) then
+      if Rule_Used (Paths, Case_Cat) /= (Control_Kinds => False) then
          Process_Min_Paths;
       end if;
 
-      if Rule_Used (Others_Span) /= (Control_Kinds => False) then
+      if Rule_Used (Others_Span, Case_Cat) /= (Control_Kinds => False) then
          Process_Min_Others_Range;
       end if;
    end Process_Case_Statement;
@@ -368,24 +410,15 @@ package body Rules.Case_Statement is
       use Asis.Elements, Asis.Statements;
       use Framework.Reports, Utilities;
 
-      Choices : constant Asis.Element_List := Case_Statement_Alternative_Choices (Path);
-      Nb_Val  : Extended_Biggest_Natural;
-      Is_Enum : constant Boolean := Type_Category (Case_Expression (Enclosing_Element (Path)),
-                                                   Follow_Derived => True,
-                                                   Privacy        => Follow_User_Private) = An_Enumeration_Type;
-      Good_Rule : Subrules;
+      Choices  : constant Asis.Element_List := Case_Statement_Alternative_Choices (Path);
+      Nb_Val   : Extended_Biggest_Natural;
+      Case_Cat : constant Categories := Matching_Category (Case_Expression (Enclosing_Element (Path)),
+                                                           From_Cats          => Discrete_Set,
+                                                           Follow_Derived     => True,
+                                                           Privacy            => Follow_User_Private,
+                                                           Separate_Extension => False);
    begin
-      if Is_Enum then
-         if Rule_Used (Enumeration_Range_Span) /= (Control_Kinds => False) then
-            Good_Rule := Enumeration_Range_Span;
-         elsif Rule_Used (Range_Span) /= (Control_Kinds => False) then
-            Good_Rule := Range_Span;
-         else
-            return;
-         end if;
-      elsif Rule_Used (Range_Span) /= (Control_Kinds => False) then
-         Good_Rule := Range_Span;
-      else
+      if Rule_Used (Range_Span, Case_Cat) = (Control_Kinds => False) then
          return;
       end if;
       Rules_Manager.Enter (Rule_Id);
@@ -413,10 +446,11 @@ package body Rules.Case_Statement is
                      return;
                   end if;
 
-                  Check_Report (Good_Rule,
-                                Value   => Nb_Val,
-                                Message => "values in choice range",
-                                Elem    => Choices (C));
+                  Check_Report (Range_Span,
+                                Value    => Nb_Val,
+                                Message  => "values in choice range",
+                                Elem     => Choices (C),
+                                Elem_Cat => Case_Cat);
                else
                   Uncheckable (Rule_Id,
                                False_Negative,
