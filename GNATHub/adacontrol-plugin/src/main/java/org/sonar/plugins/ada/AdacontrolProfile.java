37e96abc3e96e8ebd432e865a421682e10f3bcfb
Quentin Dauprat <qdauprat@pactenovation.fr>
2019-07-03 16:38:37 +0200

    SonarQube: update AdaControl way (Profile)

--- GNATHub/adacontrol-plugin/src/main/java/org/sonar/plugins/ada/AdacontrolProfile.java
+++ GNATHub/adacontrol-plugin/src/main/java/org/sonar/plugins/ada/AdacontrolProfile.java
@@ -16,14 +16,17 @@
 
 package org.sonar.plugins.ada;
 
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.sonar.api.profiles.ProfileDefinition;
-import org.sonar.api.profiles.RulesProfile;
-import org.sonar.api.profiles.XMLProfileParser;
-import org.sonar.api.utils.ValidationMessages;
+import org.sonar.api.SonarRuntime;
+import org.sonar.api.server.profile.BuiltInQualityProfilesDefinition;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
-import java.io.InputStreamReader;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.InputStream;
 
 /**
  * Load the quality profile for AdaControl.
@@ -32,21 +35,48 @@ import java.io.InputStreamReader;
  * It activates all known rules by default. See /default-profile.xml for more details.
  */
 @Slf4j
-@AllArgsConstructor
-public class AdacontrolProfile extends ProfileDefinition {
-  private final XMLProfileParser xmlLoader;
-
+public class AdacontrolProfile implements BuiltInQualityProfilesDefinition {
+  public static final String ADACONTROL_WAY_PROFILE = "AdaControl way";
   public static final String RULES_PROFILE_FILE = "/adacontrol-profile.xml";
 
-  @Override
-  public RulesProfile createProfile(final ValidationMessages messages) {
-    log.debug("Loading XML profile file: {}", RULES_PROFILE_FILE);
-    final InputStreamReader reader = new InputStreamReader(
-        AdacontrolProfile.class.getResourceAsStream(RULES_PROFILE_FILE));
+  private final SonarRuntime sonarRuntime;
 
-    final RulesProfile adactlProfile = xmlLoader.parse(reader, messages);
-    adactlProfile.setDefaultProfile(false);
+  public AdacontrolProfile(SonarRuntime sonarRuntime) {
+    this.sonarRuntime = sonarRuntime;
+  }
 
-    return adactlProfile;
+  /**
+   * Read default-profile.xml and activate all rules for the built in Quality profile.
+   * @param profile  built in quality profile
+   * @param fileName the default profile XML file
+   */
+  private void readXMLFile(NewBuiltInQualityProfile profile, String fileName) {
+    try {
+      log.debug("Loading XML profile file: {}", fileName);
+      InputStream fXmlFileStream = this.getClass().getResourceAsStream(fileName);
+      DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+      DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+      Document doc = dBuilder.parse(fXmlFileStream);
+      doc.getDocumentElement().normalize();
+      NodeList nList = doc.getElementsByTagName("rule");
+      for (int ruleIdx = 0; ruleIdx < nList.getLength(); ruleIdx++) {
+        Node nNode = nList.item(ruleIdx);
+        if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+          Element eElement = (Element) nNode;
+          String ruleName = eElement.getElementsByTagName("key").item(0).getTextContent();
+          String repoName = eElement.getElementsByTagName("repositoryKey").item(0).getTextContent();
+          profile.activateRule(repoName, ruleName);
+        }
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void define(Context context) {
+    NewBuiltInQualityProfile profile = context.createBuiltInQualityProfile(ADACONTROL_WAY_PROFILE, "ada");
+    profile.setDefault(false);
+    readXMLFile(profile, RULES_PROFILE_FILE);
+    profile.done();
   }
 }
